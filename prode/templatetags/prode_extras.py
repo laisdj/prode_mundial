171c9fb3d307aba3dffc3c7eba0e92d6ae68c4fb
laisdj <klm.miranda@gmail.com>
2026-05-31 19:05:44 -0400

    upgrade27

--- prode/templatetags/prode_extras.py
+++ prode/templatetags/prode_extras.py
@@ -4,4 +4,11 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(int(key))
+    try:
+        return dictionary.get(int(key))
+    except (ValueError, TypeError):
+        return None
+
+@register.filter
+def split(value, delimiter):
+    return value.split(delimiter)
