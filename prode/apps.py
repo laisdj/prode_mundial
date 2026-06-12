6174839bffd853a76fa6e619667fc05fbc784dce
laisdj <klm.miranda@gmail.com>
2026-06-11 21:43:05 -0400

    upgrade59

--- prode/apps.py
+++ prode/apps.py
@@ -6,6 +6,10 @@ class ProdeConfig(AppConfig):
     name = 'prode'
 
     def ready(self):
-        pass
-        # from .scheduler import start
-        # start()
+        import os
+        if os.environ.get('RUN_MAIN') or os.environ.get('RAILWAY_ENVIRONMENT'):
+            try:
+                from .scheduler import start
+                start()
+            except Exception as e:
+                print(f'Scheduler no pudo iniciar: {e}')
